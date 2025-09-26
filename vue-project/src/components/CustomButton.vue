329ab6e5f33cf6ece55479c4cdf40a7325e88a08
lea905 <leabarnezet@gmail.com>
2025-09-26 17:45:40 +0200

    Fin du cours 26/09 modification du hello word par defaut

---
+++ vue-project/src/components/CustomButton.vue
@@ -0,0 +1,16 @@
+
+<template>
+    
+     <Button label="Réinitialiser"></Button>
+
+     {{ msg }}
+</template>
+
+<script setup lang="ts">
+import Button from 'primevue/button';
+
+let props = defineProps<{msg: string}>();
+
+
+
+</script>
