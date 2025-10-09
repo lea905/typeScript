b40704e9323fd6f14568cbe11cf87e82bd46bdc5
lea905 <leabarnezet@gmail.com>
2025-10-09 16:11:23 +0200

    [ADD] primeicons

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,52 +1,46 @@
-
-  
 <template>
-    <div class="card flex justify-center">
-        <Menu :model="items">
-            <template #item="{ item, props }">
-                <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
-                    <a v-ripple :href="href" v-bind="props.action" @click="navigate">
-                        <span class="ml-2">{{ item.label }}</span>
-                    </a>
-                </router-link>
-                <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
-                    <span class="ml-2">{{ item.label }}</span>
-                </a>
-            </template>
-        </Menu>
-    </div>
-      <RouterView></RouterView>
+  <div class="card flex justify-center">
+    <Menu :model="items">
+      <template #item="{ item, props }">
+        <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
+          <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+            <i :class="item.icon"></i>
+            <span class="ml-2">{{ item.label }}</span>
+          </a>
+        </router-link>
+        <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
+          <i :class="item.icon"></i>
+          <span class="ml-2">{{ item.label }}</span>
+        </a>
+      </template>
+    </Menu>
+  </div>
+  <RouterView></RouterView>
 </template>
 
 <script setup>
-import { ref } from "vue";
-import { useRouter } from 'vue-router';
-import Menu from 'primevue/menu';
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import Menu from 'primevue/menu'
+import 'primeicons/primeicons.css'
 
-const router = useRouter();
+const router = useRouter()
 
 const items = ref([
-    {
-        label: 'Authors',
-        route: '/authors',
-        icon: 'pi pi-users'
-    },
-    {
-        label: 'Books',
-        route: '/books',
-        icon: 'pi pi-book'
-    },
-    {
-        label: 'Bookk Collections',
-        route: '/book-collections',
-        icon: 'pi pi-bookmark'
-    }
-]);
+  {
+    label: 'Authors',
+    route: '/authors',
+    icon: 'pi pi-users',
+  },
+  {
+    label: 'Books',
+    route: '/books',
+    icon: 'pi pi-book',
+  },
+  {
+    label: 'Bookk Collections',
+    route: '/book-collections',
+    icon: 'pi pi-bookmark',
+  },
+])
 </script>
-
-
-
-
-
-
-
