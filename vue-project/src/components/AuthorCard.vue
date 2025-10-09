04925c40ba3fe33ef4bd644a3a4791760c4ec2a8
lea905 <leabarnezet@gmail.com>
2025-10-09 13:44:20 +0200

    [ADD] Dialog edit button

--- vue-project/src/components/AuthorCard.vue
+++ vue-project/src/components/AuthorCard.vue
@@ -1,23 +1,62 @@
 <template>
-  <InputText v-model="localAuthor.firstName"></InputText>
-  <InputText v-model="localAuthor.name"></InputText>
+   <Dialog
+          header="Modifier un auteur"
+          v-model:visible="visible"
+          :style="{ width: '30vw' }"
+        >
+          <div class="p-fluid">
+            <div class="field">
+              <label for="firstName">Prénom</label>
+              <InputText id="firstName" v-model="localAuthor.firstName" />
+            </div>
 
-  <Button label="Sauvegarder" @click="emit('update:author', localAuthor)"></Button>
+            <div class="field">
+              <label for="name">Nom</label>
+              <InputText id="name" v-model="localAuthor.name" />
+            </div>
+          </div>
+
+          <template #footer>
+            <Button
+              label="Annuler"
+              severity="danger"
+              @click="visible = false"
+            />
+            <Button
+              label="Enregistrer"
+              @click="saveDialogAuthor"
+              autofocus
+            />
+          </template>
+        </Dialog>
 </template>
 
 <script setup lang="ts">
 import type { AuthorModel } from '@/models/Author.model';
-import { onMounted, ref, watch, type Ref } from 'vue';
+import { onMounted, ref, type Ref } from 'vue';
 import { InputText, Button } from 'primevue';
-const props = defineProps<{ author: AuthorModel }>();
+import Dialog from "primevue/dialog"
+
+
+const props = defineProps<{ author: AuthorModel}>();
+
+const visible = defineModel<boolean>('visible');
+
 const localAuthor: Ref<AuthorModel> = ref({
   id: 0,
   firstName: '',
   name: '',
 });
-const emit = defineEmits(['update:author']);
+
+const emit = defineEmits(['update:author', 'close']);
 
 onMounted(() => {
   localAuthor.value = { ...props.author };
 });
+
+const saveDialogAuthor = () => {
+    emit('update:author', localAuthor.value);
+    emit('close');
+}
+
 </script>
--- vue-project/src/components/AuthorPage.vue
+++ vue-project/src/components/AuthorPage.vue
@@ -9,43 +9,12 @@
           label="Edit"
           @click="openDialog(slotProps.data)"
         />
-
-    
-        <Dialog
-          header="Modifier un auteur"
-          v-model:visible="visible"
-          :modal="true"
-          :style="{ width: '30vw' }"
-        >
-          <div class="p-fluid">
-            <div class="field">
-              <label for="firstName">Prénom</label>
-              <InputText id="firstName" v-model="selectedAuthor.firstName" />
-            </div>
-
-            <div class="field">
-              <label for="name">Nom</label>
-              <InputText id="name" v-model="selectedAuthor.name" />
-            </div>
-          </div>
-
-          <template #footer>
-            <Button
-              label="Annuler"
-              @click="visible = false"
-              severity="danger"
-            />
-            <Button
-              label="Enregistrer"
-              @click="saveDialogAuthor"
-              autofocus
-            />
-          </template>
-        </Dialog>
-
       </template>
     </Column>
   </DataTable>
+
+  <AuthorCard v-if="visible" :author="selectedAuthor" v-model:visible="visible" @update:author="updateAuthor($event)" />
+  
 </template>
 
 <script setup lang="ts">
@@ -53,8 +22,8 @@ import { ref, type Ref } from "vue"
 import DataTable from "primevue/datatable"
 import Column from "primevue/column"
 import Button from "primevue/button"
-import Dialog from "primevue/dialog"
-import InputText from "primevue/inputtext"
+import AuthorCard from "./AuthorCard.vue"   
+
 import type { AuthorModel } from "@/models/Author.model"
 
 const authors = ref<AuthorModel[]>([
@@ -63,24 +32,26 @@ const authors = ref<AuthorModel[]>([
   { id: 2, firstName: "Victor", name: "Hugo" },
 ])
 
-const visible = ref(false)
 const selectedAuthor: Ref<AuthorModel> = ref({
   id: -1,
   firstName: "",
   name: "",
 })
 
+const visible = ref(false)
 
 const openDialog = (author: AuthorModel) => {
   selectedAuthor.value = { ...author }
   visible.value = true
 }
 
-const saveDialogAuthor = () => {
-  const index = authors.value.findIndex((a) => a.id === selectedAuthor.value.id)
+const updateAuthor = (newAuthor: AuthorModel) => {
+  const index = authors.value.findIndex((a) => a.id === newAuthor.id)
   if (index !== -1) {
-    authors.value[index] = { ...selectedAuthor.value }
+    authors.value[index] = { ...newAuthor }
   }
   visible.value = false
-}
+}   
+
+
 </script>
